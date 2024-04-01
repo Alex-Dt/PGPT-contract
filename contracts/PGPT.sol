077501734b1ea7a6bc076189c0ff005bb182eb86
Alex-Dt <aleksejs.dt@gmail.com>
2024-04-01 13:23:25 +0200

    added cap of 100M tokens

--- contracts/PGPT.sol
+++ contracts/PGPT.sol
@@ -6,8 +6,10 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PGPT is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("PGPT", "PGPT") Ownable(msg.sender) {
-        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+    uint256 private constant MAX_SUPPLY = 100_000_000 * 10**6; // 100 million tokens, assuming 6 decimals
+
+    constructor() ERC20("PrivateAI.com", "PGPT") Ownable(msg.sender) {
+        _mint(msg.sender, 100_000_000 * 10**decimals());
     }
 
     function decimals() public view virtual override returns (uint8) {
@@ -15,6 +17,7 @@ contract PGPT is ERC20, ERC20Burnable, Ownable {
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds maximum supply");
         _mint(to, amount);
     }
 }
