5b9b12aa0e0cf5c4e52e4945b82801aeb14db734
Andrey <systerr@gmail.com>
2024-04-01 16:17:54 +0400

    deps

--- test/PGPT.js
+++ test/PGPT.js
@@ -19,12 +19,12 @@ describe("PGPT Token", function () {
     expect(await pgptT.decimals()).to.equal(6);
   });
 
-  it("Should have 100 bln of tokens ", async function () {
+  it("Should have 33 bln of tokens ", async function () {
     const [owner] = await ethers.getSigners();
     const decimals = await pgptT.decimals();
 
     expect(await pgptT.balanceOf(owner.address)).to.equal(
-      100_000_000n * 10n ** BigInt(decimals)
+      33_333_333n * 10n ** BigInt(decimals)
     );
   });
   it("Should have ability to burn  tokens ", async function () {
@@ -33,7 +33,7 @@ describe("PGPT Token", function () {
     await pgptT.burn(1_000_000n * 10n ** BigInt(decimals));
 
     expect(await pgptT.balanceOf(owner.address)).to.equal(
-      99_000_000n * 10n ** BigInt(decimals)
+      32_333_333n * 10n ** BigInt(decimals)
     );
   });
 
@@ -43,7 +43,7 @@ describe("PGPT Token", function () {
     await pgptT.mint(owner.address, 1_000_000n * 10n ** BigInt(decimals));
 
     expect(await pgptT.balanceOf(owner.address)).to.equal(
-      100_000_000n * 10n ** BigInt(decimals)
+      33_333_333n * 10n ** BigInt(decimals)
     );
   });
 
